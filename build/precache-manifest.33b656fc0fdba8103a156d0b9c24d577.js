52170c785bce083b8adeb31e054b9b886103bd08
aditya choudhary <adityac937@gmail.com>
2020-08-18 14:40:06 +0530

    Ordering App

---
+++ build/precache-manifest.33b656fc0fdba8103a156d0b9c24d577.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bf723e573b3bbcf0747b7a8c64a41a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65ef864ffbe29d507114",
+    "url": "/static/css/main.2935df98.chunk.css"
+  },
+  {
+    "revision": "74821c379a89d5c9e901",
+    "url": "/static/js/2.00dc15c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.00dc15c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ef864ffbe29d507114",
+    "url": "/static/js/main.9e8ed24a.chunk.js"
+  },
+  {
+    "revision": "a8c672de2dc0c57592e7",
+    "url": "/static/js/runtime-main.89b712eb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
